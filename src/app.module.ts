14deb45d84cd5e9c3b6ab4aad8f4a3534b714742
Reynaldo Ruiz <d9@tkiero.app>
2024-01-15 19:53:17 -0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { SeedModule } from './modules/seed/seed.module';
+
+import { AuthModule } from './modules/auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    SeedModule,
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
